73df9af12e6591537196c2be6b89cf64843c162f
= <linne.louise@gmail.com>
2023-03-21 00:11:56 +0100

    Add events

---
+++ displayEvents.js
@@ -0,0 +1,21 @@
+let lecturesContainer = document.getElementById('lectures');
+let trainingsContainer = document.getElementById('trainings');
+
+
+let res = fetch("./events.json")
+    .then((response) => response.json())
+    //.then((json) => console.log(json))
+    .then((json) => {
+        const lectures = json.lectures;
+        for (let i = 0; i < lectures.length; i++) {
+            let element = document.createElement("div");
+            element.innerHTML = `<p><a href="${lectures[i]["url"]}">${lectures[i]["place"]}</a>: ${lectures[i]["time"]}</p>`;
+            lecturesContainer.appendChild(element);
+        };
+        const trainings = json.trainings;
+        for (let i = 0; i < trainings.length; i++) {
+            let element = document.createElement("div");
+            element.innerHTML = `<p>${trainings[i]["place"]}: ${trainings[i]["time"]}</p>`;
+            trainingsContainer.appendChild(element);
+        };
+    });
